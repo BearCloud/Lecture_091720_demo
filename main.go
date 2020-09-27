7a130b7ef91f58b118717ff56302b898d99b5a45
Ari Rigan <ari.rigan@gmail.com>
2020-09-26 19:10:49 -0700

    added comments

--- main.go
+++ main.go
@@ -2,51 +2,91 @@ package main
 
 
 import (
+
+	// fmt allows us to print to our command line to debug our program.
 	"fmt"
-	_"os"
-	_"github.com/joho/godotenv"
+
+	//os allows us to access the operating systems current environment.
+	"os"
+
+	//godotenv allows us to load environment variables from a file into our current operating system's environment.
+	"github.com/joho/godotenv"
+
+	/*
+		http gives us access to all networking libaries associated with http.
+		Theoretically this is the only library we need to create a backend service, but gorilla/mux makes it a LOT easier.
+	*/
 	"net/http"
+
+	//gorilla/mux builds on top of net/http to make it easier to create endpoints.
 	"github.com/gorilla/mux"
+
+	//json lets us take the json bodies from incoming requests and then decode them into a Golang object.
 	"encoding/json"
+
+	//strconv lets us convert any primitive data type (integer, string, boolean) into its string equivalent.
+	//This allows us to convert our local data types to string when we try and output them in our HTTP responses
 	"strconv"
 )
 
+/*
+	We want to create a "blueprint" of a rectangle so that any function that wants to create a rectangle knows how.
+	Every rectangle blueprint has its length and width. Both are type integer.
+	the "json" tags at the end lets the "encoding/json" library know that any json keys
+*/
 type Rectangle struct {
 	Length int `json:"length"`
 	Width int `json:"Width"`
 }
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
+func helloWorld(response http.ResponseWriter, request *http.Request) {
 	
-	//query parameters
-	username := r.URL.Query().Get("username")
-	password := r.URL.Query().Get("password")
+	//Obtain the query paramters with keys "username" and "password"
+	username := request.URL.Query().Get("username")
+	password := request.URL.Query().Get("password")
 
-	fmt.Fprintf(w, username +"\n")
-	fmt.Fprintf(w, password +"\n")
+	
+	//Print the values of the query parameter to the response ouput
+	//If we sent out request from insomnia, these are the values we will see
+	fmt.Fprintf(response, username +"\n")
+	fmt.Fprintf(response, password +"\n")
 
 
-	//json body
+	//Take the JSON request body in the request we received and populate our empty Rectangle with the values
+
+	//create an empty rectangle
 	rectangle := Rectangle{}
-	err := json.NewDecoder(r.Body).Decode(&rectangle)
+
+	//create a new json decoder that will allow us to decode the request body
+	jsonDecoder := json.NewDecoder(request.Body)
+
+	//use our decoder to decode the contents of the request body into our rectangle
+	err := jsonDecoder.Decode(&rectangle)
+
+	//Check if we got an error. If err != nil, that function returned an error
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(response, err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(w, strconv.Itoa(rectangle.Length) + "\n")
-	fmt.Fprintf(w, strconv.Itoa(rectangle.Width) + "\n")
+
+	//print out our rectangle dimensions to the response output
+	//If we sent out request from insomnia, these are the values we will see
+	fmt.Fprintf(response, strconv.Itoa(rectangle.Length) + "\n")
+	fmt.Fprintf(response, strconv.Itoa(rectangle.Width) + "\n")
 
 
-	//cookie
-	cookie, err := r.Cookie("access_token")
+	//Obtain the the cookie "access_token", this gives us all the chararcteristics of the cookie
+	cookie, err := request.Cookie("access_token")
 
+	//check if obtaining the cookie returned an error
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(response, err.Error(), http.StatusBadRequest)
 		return
 	}
 	
+	//Get the value of the cookie we just obtained and print it out to the response output
 	accessToken := cookie.Value
-	fmt.Fprintln(w, accessToken)
+	fmt.Fprintln(response, accessToken)
 
 	return
 }
@@ -54,25 +94,29 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	// //get command line arguments
-	// args := os.Args
+	//get command line arguments
+	args := os.Args
 
-	// fmt.Println(args)
+	fmt.Println(args)
 
-	// //environment variables
-	// err :=  godotenv.Load()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	//load the environment variables from the .env file into our program's environment
+	err :=  godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	// secretToken := os.Getenv("SECRET_TOKEN")
-	// fmt.Println(secretToken)
+	//Get the "SECRET_TOKEN" variable and store it in a local Golang variable
+	secretToken := os.Getenv("SECRET_TOKEN")
+	fmt.Println(secretToken)
 
-	//server programming
+	//Create a router that matches urls to their specific functions
 	router := mux.NewRouter()
 
-	router.HandleFunc("/helloworld", helloWorld).Methods(http.MethodGet, http.MethodPost)
 
+	//Forward every request with url "localhost:8080/helloworld" to the helloWorld function. The request method must be GET.
+	router.HandleFunc("/helloworld", helloWorld).Methods(http.MethodGet)
+
+	//Begin listening for requests
 	http.ListenAndServe(":8080", router)
 
 
